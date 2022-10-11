25db18742e45f1784db50cbdd43a2b53067b732e
wilfredomorillo <wilfredo.morillo@hotmail.com>
2022-10-10 20:50:18 -0400

    primero

--- checkpoint.js
+++ checkpoint.js
@@ -5,7 +5,9 @@ function menorMayor(numeros) {
   // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
   // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
   // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
-
+let max= Math.max(...numeros)
+let min= Math.min(...numeros)
+return [min, max]
 }
 
 function stringMasLarga(strings) {
@@ -14,7 +16,13 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
+let resultado =''
+for (let i = 0; i < strings.length; i++) {
+  if (strings[i].length>resultado.length)
+  resultado=strings[i]
+  
+}
+return resultado
 }
 
 function buscarAmigo(amigos, nombre) {
@@ -24,7 +32,12 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
+
+for (let i = 0; i < amigos.length; i++) {
+  if (amigos[i].nombre=== nombre) return amigos[i]
   
+}
+
 }
 
 function sumArray(array, n) {
@@ -35,7 +48,20 @@ function sumArray(array, n) {
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
+  for (let i = 0; i < array.length; i++) {
   
+    for (let j = 0; j < array.length; j++) {
+     if (i===j) continue 
+      if(array[i]+array[j]=== n) return true
+        
+      
+     
+    }
+   
+      
+  }
+return false
+ 
 };
 
 function pluck(array, propiedad) { 
@@ -46,8 +72,8 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
-  
+let respuesta = array.map((a)=>a[propiedad])
+return respuesta
 }
 
 // =======================================================================
@@ -61,35 +87,46 @@ function crearClasePersona() {
 
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
-      
+      this.nombre= nombre
+      this.edad=edad
+      this.hobbies=hobbies
+      this.amigos=amigos
+      this.Persona= function(){
+        nombre= this.nombre
+        edad= this.edad
+        hobbies= this.hobbies
+        amigos= this.amigos
+      }
+  
     }
 
     addFriend(nombre, edad) {
       // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // no debe retornar nada.
-      
+   this.amigos.push({nombre , edad})
     }
 
     addHobby(hobby) {
       // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
       // no debe retornar nada.
-      
+    this.hobbies.push(hobby)
     }
     getFriends() {
       // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
-      
-    }
+  let respuesta= this.amigos.map((a)=>a.nombre)
+return respuesta    
+}
 
     getHobbies() {
       // Escribe una función que retorne un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
-    }
+return this.hobbies
+      }
 
     getPromedioEdad() {
       // Escribe una función que retorne el promedio de edad de los amigos de una persona
@@ -104,7 +141,11 @@ function crearClasePersona() {
       //   }]
       // }
       // persona.getPromedioEdad() // retorna 29
-    }
+
+let respuesta = this.amigos.map((e)=>e.edad).reduce((a,b)=> a+b)/this.amigos.length
+return respuesta
+ }
+
   };
 
   return Persona;
--- teoria.md
+++ teoria.md
@@ -1,14 +1,14 @@
 ## Explica los siguentes conceptos con tus propias palabras. (no más de tres líneas por respuesta).
 
-* HTML:
+* HTML:esqueleto donde vamos a vinvular nuestros componentes y css
 
-* CSS:
+* CSS: aqui vamos a poner lindo todo esto 
 
-* Git:
+* Git: ternimal q nos permite pushear archivos en la nuve y bueno una ternimal 
 
-* Arreglos:
+* Arreglos: son objetos representados con cajitas[]
 
-* Objetos:
+* Objetos: es de lo q se compone el js 
 
 * Variables:
 
